d9355be1d2a6b257dc8988a222fe4fc4e210d088
Weilai Xiang <xiangweilai99@gmail.com>
2017-12-21 21:51:31 +0800

    update

--- hw7/monkey.c
+++ hw7/monkey.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+int f[1003];
+int n,m,q;
+int check()
+{
+    int i,cnt=0,flag=-1;
+    for(i=1;i<=n;i++)
+    {
+	cnt+=f[i];
+	if(f[i]) flag=i;
+    }
+    if(cnt==1) return flag;
+    else return 0;
+}
+int find_next(int x)
+{
+    while(++x)
+    {
+	if(x==n+1) x=1;
+	if(f[x]) return x;
+    }
+}
 int main()
 {
-    int n,m,q;
     scanf("%d%d%d",&n,&m,&q);
-
+    int i,j,ck;
+    for(i=1;i<=n;i++) f[i]=1;
+    q=(q==1)?n:q-1;
+    while(1)
+    {
+	if(ck=check())
+	{
+	    printf("%d\n",ck);
+	    return 0;
+	}
+	for(j=1;j<=m;j++) q=find_next(q);
+	f[q]=0;
+    }
     return 0;
 }
